590fcd1b7ba9c40d67d09debd0d80a9d1b56fe4c
chonlawat <chonlawat.bowonwongpitag@marlo.com.au>
2018-10-17 16:39:14 +0700

    no message

--- src/main/java/com/dekses/jersey/docker/demo/MyResource.java
+++ src/main/java/com/dekses/jersey/docker/demo/MyResource.java
@@ -8,7 +8,8 @@ import javax.ws.rs.core.MediaType;
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+//@Path("myresource")
+@Path("/")
 public class MyResource {
 
     /**
